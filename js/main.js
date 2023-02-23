65d9039bb7afe7474cd2c09bfc9b690f581ec38d
AlejoFernandez10 <alejofernandez1239@gmail.com>
2023-02-22 21:05:22 -0300

    Carrito finalizado

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const carritoIcon = document.getElementById(`carritoIcon`)
 const mainContainer = document.getElementById(`mainContainer`)
 const mainTitle = document.getElementById(`clasesTitle`);
 
+const cartItemContainer = document.getElementById(`cartItemContainer`)
+
 
 const efectoEscritura = (text = '', time = 0, etiqueta = '') => {
 
@@ -43,11 +45,15 @@ efectoEscritura(`TENEMOS TODO LO QUE BUSCÁS`, 80, mainTitle);
 
 //EFECTO on-scroll NAV
 
+const header = document.getElementById(`header`);
 
 window.addEventListener("scroll", ()=>{
-    const header = document.getElementById(`header`);
+    header.style.backgroundColor = `#000 !important`
     header.classList.toggle("reaparecer", window.scrollY > 500);
+    
 })
+
+
 
 
 
@@ -67,18 +73,75 @@ const adquirirBtn = document.querySelectorAll(`.adquirir-planBtn`);
 adquirirBtn.forEach(btn =>{
 
     btn.addEventListener(`click`,()=>{
-        carritoCounter.innerHTML ++;
+        carritoCounter.innerHTML = 1;
         Swal.fire({
             title: 'Plan agregado!',
             text: `Su plan fue agregado al carrito correctamente.`,
             icon: 'success',            
-            timer:1000,
+            
           })
 
+          if(sessionStorage.getItem(`plan`) != null | sessionStorage.getItem(`plan`) != undefined){
+            Swal.fire({
+                title: 'Plan reemplazado',
+                text: `Su carrito fue actualizado correctamente.`,
+                icon: 'success',            
+                
+              })
+        }
 
+        sessionStorage.setItem( `plan`,btn.parentElement.parentElement.firstElementChild.innerText )       
+        sessionStorage.setItem(`planPrecio`,btn.parentElement.parentElement.lastElementChild.previousElementSibling.firstElementChild.innerText)
+                
+        
+
+        const carritoItem = `
+                        <div class="card-body p-4" >
+                                          <div class="row d-flex justify-content-between align-items-center">
+                                            <div class="col-md-2 col-lg-2 col-xl-2">
+                                              <img
+                                                src="./assets/gym-equipment (1).png"
+                                                class="img-fluid rounded-3" alt="plan">
+                                            </div>
+                                            <div class="col-md-3 col-lg-3 col-xl-4">
+                                              <p class="lead fw-normal mb-2" id="plan-title-cart">${sessionStorage.getItem(`plan`)}</p>
+
+                                            </div>
+
+                                            <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                                              <h5 class="mb-0">${sessionStorage.getItem(`planPrecio`)}</h5>
+                                            </div>
+                                            <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                                              <a href="#!" class="text-danger" onclick="vaciarCarrito()"><img src="./assets/bin.png" alt="" ></a>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+
+                        `
+
+        cartItemContainer.innerHTML = carritoItem;
+
+        
     })
 
 })
 
 
-let carrito = []
+const vaciarCarrito = ()=>{
+    sessionStorage.clear()
+    carritoCounter.innerHTML = 0
+    cartItemContainer.innerHTML = ` `;
+    
+    Swal.fire({
+        title: 'Carrito vacío',
+        text: `El carrito se vació correctamente`,
+        icon: 'success',            
+        
+      })
+}
+
+/* console.log(sessionStorage.getItem(`planPrecio`))
+ */
+
+
